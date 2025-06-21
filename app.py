9c067fd0e75de6b337fcebeb8dbdd970693215ad
Mateo Vallejo <mateovallejo@MacBook-Pro-de-Mateo-2.local>
2025-06-21 01:14:03 -0300

    l

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ Mensaje del usuario:
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))  # Railway setea PORT como env var
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
