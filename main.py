8eef5817ab54aab3cef5ec2f95fd6813fe0eba09
Mateo Vallejo <mateovallejo@MacBook-Pro-de-Mateo-2.local>
2025-06-21 15:07:42 -0300

    t

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def scrapeo():
 
 # 4. Buscar cada card de carrera
     target_substring = "col-xs-12 col-sm-6 col-md-4 col-lg-3 cajaIndividual element-item"
-    cards = soup.findcards = soup.find_all("div", class_=lambda c: c and target_substring in " ".join(c if isinstance(c, list) else [c]))
+    cards = soup.find_all("div", class_=lambda c: c and target_substring in " ".join(c if isinstance(c, list) else [c]))
     print(f"🔍 Se encontraron {len(cards)} cards de carreras")
 
     for index, card in enumerate(cards):
